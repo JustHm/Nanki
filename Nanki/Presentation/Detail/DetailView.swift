6e427ec5a10896c32ffedb8e531e5afd50a0f3e2
JungHm <taggma12@gmail.com>
2025-02-18 12:30:48 +0900

    Feat: HomeView 기존 작업 추가

--- Nanki/Presentation/Detail/DetailView.swift
+++ Nanki/Presentation/Detail/DetailView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct DetailView: View {
+    @Binding var list: WordSet
+    @State private var isAdded: Bool = false
+    @State private var addsheet: Bool = false
+    @State private var title: String
+    var isCanEdit: Bool
+    
+    init(list: Binding<WordSet>, isCanEdit: Bool) {
+        //Property Wrapper가 달려있는 변수의경우 _를 붙여서 접근할 수 있다.
+        //State를 기본값을 받아 초기화 하는경우 State(initialValue:) 함수를 사용해 초기화할 수 있다.
+            _list = list
+            _title = State(initialValue: list.wrappedValue.title)
+            self.isCanEdit = isCanEdit
+    }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-#Preview {
-    DetailView()
-}
+//#Preview {
+//    DetailView()
+//}
--- Nanki/Presentation/Home/HomeView.swift
+++ Nanki/Presentation/Home/HomeView.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct HomeView: View {
+    @EnvironmentObject var store: WordStore // WordStore 환경 객체
+    @State var pickerSelected: HomePicker = .Custom
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section {
+                Picker("Picker", selection: $pickerSelected) {
+                    ForEach(HomePicker.allCases, id: \.self) {
+                        Text($0.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .listRowBackground(Color.clear)
+            ForEach(currentWordSet()) { item in
+                HomeCell(item: item, isCustom: (pickerSelected == .Custom))
+            }
+            .onDelete { index in
+                store.deleteWordSet(indexSet: index)
+            }
+            .deleteDisabled(pickerSelected == .Gallery)
+        }
+        .listStyle(.sidebar)
+        .navigationTitle("NanKi")
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Add" ,systemImage: "plus") {
+                    withAnimation { store.addNewWordSet() }
+                }
+                .disabled(pickerSelected == .Gallery)
+            }
+        }
+        .navigationBarTitleDisplayMode(.large)
+    }
+    
+    enum HomePicker: String, CaseIterable {
+        case Custom, Gallery
+    }
+    private func currentWordSet() -> Binding<[WordSet]>{
+        pickerSelected == .Custom ? $store.wordList : $store.gallery
     }
 }
 
 #Preview {
-    HomeView()
+    NavigationView {
+        HomeView()
+            .environmentObject(WordStore())
+    }
 }
