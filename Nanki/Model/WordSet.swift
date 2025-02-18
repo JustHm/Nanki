2a0ca976255a29a8700c559e8ffde04276a74d51
JungHm <taggma12@gmail.com>
2025-02-18 10:54:07 +0900

    프로젝트 초기 세팅

---
+++ Nanki/Model/WordSet.swift
@@ -0,0 +1,8 @@
+//
+//  WordSet.swift
+//  Nanki
+//
+//  Created by 안정흠 on 2/18/25.
+//
+
+import Foundation
---
+++ Nanki/Presentation/WordStore.swift
@@ -0,0 +1,8 @@
+//
+//  WordStore.swift
+//  Nanki
+//
+//  Created by 안정흠 on 2/18/25.
+//
+
+import Foundation
