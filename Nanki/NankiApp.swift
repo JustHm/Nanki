0b7398514e4b801f1eb96c39e071042aadcc55d4
JungHm <taggma12@gmail.com>
2025-02-18 10:50:44 +0900

    Initial Commit

---
+++ Nanki/NankiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NankiApp.swift
+//  Nanki
+//
+//  Created by 안정흠 on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct NankiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
