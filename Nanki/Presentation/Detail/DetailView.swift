628cdf80523df44b8c4b5713b370cc01ab618b6d
lkhwan0204 <lkhwan0204@gmail.com>
2025-02-18 13:13:06 +0900

    디테일 뷰 추가

--- Nanki/Presentation/Detail/DetailView.swift
+++ Nanki/Presentation/Detail/DetailView.swift
@@ -17,16 +17,75 @@ struct DetailView: View {
     init(list: Binding<WordSet>, isCanEdit: Bool) {
         //Property Wrapper가 달려있는 변수의경우 _를 붙여서 접근할 수 있다.
         //State를 기본값을 받아 초기화 하는경우 State(initialValue:) 함수를 사용해 초기화할 수 있다.
-            _list = list
-            _title = State(initialValue: list.wrappedValue.title)
-            self.isCanEdit = isCanEdit
+        _list = list
+        _title = State(initialValue: list.wrappedValue.title)
+        self.isCanEdit = isCanEdit
     }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            List {
+                Section(footer: (isCanEdit) ? Text("제목 수정가능.") : Text("")) {
+                    VStack {
+                        TextField("제목을 입력하세요", text: $title)
+                            .font(.largeTitle)
+                            .bold()
+                            .disabled(!isCanEdit)
+                            .onSubmit {
+                                list.title = title
+                            }
+                        Divider().background(.blue)
+                    }
+                }
+                .listRowInsets(EdgeInsets())
+                .listRowBackground(Color.clear)
+                
+                Section {
+                    NavigationLink {
+                    } label: {
+                        Text("단어퀴즈")
+                    }
+                    .disabled(list.wordList.isEmpty)
+                }
+                Section("단어 (list.wordList.count)개") {
+                    ForEach(list.wordList, id: \.title) { item in
+                        Text(item.title)
+                    }
+                    .onDelete { index in
+                        withAnimation {
+                            list.wordList.remove(atOffsets: index)
+                        }
+                    }
+                    .deleteDisabled(!isCanEdit)
+                }
+            }
+        }
+        .overlay(alignment: .bottomTrailing) {
+            if isCanEdit {
+                Button(action: {addsheet.toggle()}) {
+                    Image(systemName: "plus")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(.white)
+                        .padding()
+                }
+                .background(Circle().fill(Color.blue))
+                .frame(width: 64, height: 64)
+            }
+        }
+        .sheet(isPresented: $addsheet, content: {
+            NavigationView {
+            }
+            .presentationDetents([.medium])
+        })
     }
 }
-
-//#Preview {
-//    DetailView()
-//}
+#Preview {
+    @Previewable @State var list = WordSet(title: "HI", wordList: [])
+    NavigationView {
+        DetailView(list: $list,
+                   isCanEdit: true
+        )
+    }
+}
